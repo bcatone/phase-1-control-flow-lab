c8e45044a404330da51dec1a5112212e6f7306f9
Brittany Laura Catone <bcatone@outlook.com>
2022-11-19 14:57:54 -0500

    commit tree cb59ea3e19ec444c7fc569afc22ab82c16d43f39

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+
+  // Check to make sure argument is a valid distance in feet
+  let distance = parseFloat(feet);
+
+  if (distance < 0 || distance === NaN) {
+    return `It isn't even possible to travel "${feet}" feet!`;
+  } else if (distance < 400) {
+    return 'This one is on me!';
+  } else if (distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const response = (city === 'NYC') ? ('Ok, sounds good.') : ('No go.');
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generosity){
   // Write your code here!
+  switch (generosity) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
 }
